37eaab572abae1db26ee3b15cbdc0cfe173c00dc
imtiaz <imtiazkhaled07@gmail.com>
2019-09-25 18:08:54 -0500

    setup app

---
+++ src/pages/landingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class LandingPage extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className='landing-page'>
+                Hi, I'm <span className='landing-page-name'>Imtiaz Khaled</span> a {thiswho}
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
